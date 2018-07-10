d007c4e610dbf7acff13b4e274d063a97f5111f7
Deivu (Saya) <36309350+Deivu@users.noreply.github.com>
2018-07-10 17:48:35 +0800

    Async
    Forgot to Async it

--- commands/ping.js
+++ commands/ping.js
@@ -1,4 +1,4 @@
-exports.run = (client, msg) => {
+exports.run = async (client, msg) => {
 	const heartBeat = `\`\`\`Last 3 Pings:\n${client.pings[0]}ms\n${client.pings[1]}ms\n${client.pings[2]}ms\`\`\``;
-	msg.channel.send(`\:left_right_arrow: Websocket Latency: **${client.ping}ms**\n${heartBeat}`)
+	await msg.channel.send(`\:left_right_arrow: Websocket Latency: **${client.ping}ms**\n${heartBeat}`)
 };
